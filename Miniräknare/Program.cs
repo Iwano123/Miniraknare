b13f90c901bb99b8a3708dd6987d5e069fe5851e
Iwano123 <korkisiwan@gmail.com>
2024-09-30 23:26:42 +0200

    Kommenterat min kod och tagit bort en del i min if sats som inte behövdes vid division med noll.

--- Miniräknare/Program.cs
+++ Miniräknare/Program.cs
@@ -11,40 +11,54 @@ namespace Miniräknare
     {
         static void Main(string[] args)
         {
+            // Välkomstmeddelande
             Console.WriteLine("Enkel Kalkylator");
+
+            // Användaren ombeds att ange en operation (+, -, *, /)
             Console.WriteLine("Ange operation (+, -, *, /): ");
-            char operation = Console.ReadKey().KeyChar;
+            char operation = Console.ReadKey().KeyChar; // Läs in operation från användaren
             Console.WriteLine("\nAnge första talet: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine()); // Läs in första talet
             Console.WriteLine("Ange andra talet: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine()); // Läs in andra talet
 
-
+            // Variabel som lagrar resultatet av operationen
             double result = 0;
+
+            // Switch-sats för att hantera olika valen som användare kan göra i programmet
             switch (operation)
             {
                 case '+':
-                    result = num1 + num2;
+                    result = num1 + num2; // Addition
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = num1 - num2; // Subtraktion
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = num1 * num2; // Multiplikation
                     break;
                 case '/':
-                    if (num1 != 0 || num2 != 0)
+                    // Kontroll för att undvika division med noll
+                    if (num2 != 0)
                     {
-                        result = num1 / num2;
+                        result = num1 / num2; // Division
+                    }
+                    else
+                    {
+                        // Felmeddelande om användaren försöker dividera med noll
+                        Console.WriteLine("Fel: Division med noll är inte tillåten.");
+                        return;
                     }
                     break;
                 default:
+                    // Om användaren anger en ogiltig operation
                     Console.WriteLine("Ogiltig operation.");
                     return;
             }
 
+            // Visar resultatet av operationen för användaren
             Console.WriteLine($"Resultat: {result}");
-            Console.ReadKey();
+            Console.ReadKey(); // Väntar på att användaren trycker på en tangent innan programmet avslutas
         }
     }
 }
